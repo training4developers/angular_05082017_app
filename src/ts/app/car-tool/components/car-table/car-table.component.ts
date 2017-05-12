f63eff79292021d1c83e80c9f14eb4865e4766a1
Eric W. Greene <eric@training4developers.com>
2017-05-11 23:59:54 -0400

    Added table export.

--- src/ts/app/car-tool/components/car-table/car-table.component.ts
+++ src/ts/app/car-tool/components/car-table/car-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, AfterViewInit, AfterViewChecked, OnDestroy } from "@angular/core";
+import * as $ from "jquery";
+import "tableexport";
 
 import { Car } from "../../models/car";
 
@@ -6,7 +8,23 @@ import { Car } from "../../models/car";
     selector: "car-table",
     template: require("./car-table.component.html"),
 })
-export class CarTableComponent {
+export class CarTableComponent implements AfterViewInit, AfterViewChecked, OnDestroy {
+
+    private tableElement: JQuery;
+    private exportTable: any;
+
+    public ngAfterViewInit() {
+        this.tableElement = $("table");
+        this.exportTable = this.tableElement.tableExport();
+    }
+
+    public ngAfterViewChecked() {
+        this.exportTable.update();
+    }
+
+    public ngOnDestroy() {
+        this.exportTable.remove();
+    }
 
     @Input()
     public cars: Car[] = [];
